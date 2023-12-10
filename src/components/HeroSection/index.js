2a51c3a7006f1c309bef1a90f065d8d84dc4ba13
shivamdubeyagra <shivamdubeyagra@gmail.com>
2023-12-10 22:30:52 +0530

    my portfolio

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
+import HeroImg from '../../images/HeroImage.png'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+
+    const resumebtn = ()=>{
+        const pdfPath = 'Shivam-Dubey-Resume.pdf';
+        window.open(pdfPath,'_blank');
+        const downloadLink = document.createElement('a');
+        downloadLink.href = pdfPath;
+        downloadLink.download = 'Shivam-Dubey-Resume.pdf';
+        downloadLink.click();
+        }
+
+    return (
+        <div>
+            <HeroContainer >
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer id="home">
+                    <HeroLeftContainer id="Left">
+                        <Title id='user-detail-name'>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle id="user-detail-intro">{Bio.description}</SubTitle>
+                        <ResumeButton  onClick={resumebtn} target='display' id='resume-button-2'>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+                    <HeroRightContainer id="Right">
+                        <Img className='home-img' src={HeroImg} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
